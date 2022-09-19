74281a18d8ec5e500d6adefbf7f57b13ba5f3b21
lisagaudinWCS <lisagaudin.wcs@gmail.com>
2022-09-19 16:32:42 +0200

    finito challenge 8

--- app.js
+++ app.js
@@ -14,32 +14,34 @@ const welcome = (req, res) => {
 
 app.get("/", welcome);
 
+const { hashPassword, verifyPassword, verifyToken } = require("./auth");
 const movieHandlers = require("./movieHandlers");
-
+const userHandlers = require("./userHandlers");
+//public routes
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-const userHandlers = require("./userHandlers");
-
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-// app.post("/api/users", userHandlers.postUser);
-app.put("/api/users/:id", userHandlers.updateUser);
-app.delete("/api/users/:id", userHandlers.deleteUser);
 
-//EXPRESS 08
-const { hashPassword, verifyPassword } = require("./auth");
-
+// login 
 app.post(
   "/api/login",
   userHandlers.getUserByEmailWithPasswordAndPassToNext,
   verifyPassword
 );
-//EXPRESS 07
+//wall token
+app.use(verifyToken);
+// protected routes MOVIES
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+//protected routes USERS
 app.post("/api/users", hashPassword, userHandlers.postUser);
+app.put("/api/users/:id", userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
+
+
 
 
 app.listen(port, (err) => {
--- auth.js
+++ auth.js
@@ -1,8 +1,49 @@
 const argon2 = require("argon2");
+const jwt = require("jsonwebtoken");
 
+
+const verifyToken = (req, res, next) => {
+  try {
+    const authorizationHeader = req.get("Authorization");
+    if (authorizationHeader == null) {
+      throw new Error("Authorization header is missing");
+    }
+    const [type, token] = authorizationHeader.split(" ");
+
+    if (type !== "Bearer") {
+      throw new Error ("Authorization header hasn't the Bearer type");
+    }
+    
+    req.payload = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(401);
+  }
+};
 const verifyPassword = (req, res) => {
-  res.send(req.user);
-}
+  argon2
+    .verify(req.user.hashedPassword, req.body.password)
+    .then((isVerified) => {
+      if (isVerified) {
+        const payload = { sub: req.user.id };
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+
+        delete req.user.hashedPassword;
+
+        res.send({ token, user: req.user });
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
 
 const hashingOptions = {
   type: argon2.argon2id,
@@ -27,6 +68,7 @@ const hashPassword = (req, res, next) => {
 };
 
 module.exports = {
+  verifyToken,
   verifyPassword,
   hashPassword
 };
